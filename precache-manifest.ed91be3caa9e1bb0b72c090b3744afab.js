e9e02f3101737c2704aa116cf2f700909611e219
yashraj15 <yashraj.jaiswal15@gmail.com>
2021-07-27 03:00:40 -0500

    Updates

---
+++ precache-manifest.ed91be3caa9e1bb0b72c090b3744afab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8763ef41eb15afb98b7",
+    "url": "/stout-case/static/css/main.a5001779.chunk.css"
+  },
+  {
+    "revision": "a8763ef41eb15afb98b7",
+    "url": "/stout-case/static/js/main.a8763ef4.chunk.js"
+  },
+  {
+    "revision": "3fc64f5a5b1d55673fd9",
+    "url": "/stout-case/static/js/1.3fc64f5a.chunk.js"
+  },
+  {
+    "revision": "e150207d1260880f9435",
+    "url": "/stout-case/static/js/runtime~main.e150207d.js"
+  },
+  {
+    "revision": "a5fa31565006d58afc564748c3a0bc24",
+    "url": "/stout-case/index.html"
+  }
+];
